2e196ddaba874219bce24d58458f6326bd85f4b2
Ernest Mamba <fistozz.mamba@gmail.com>
2022-09-18 01:48:19 +0200

    creating folder structure

--- src/api/lotto.py
+++
@@ -1,8 +0,0 @@
-class LottoObject:
-    def __init__(self, type: str, date: dict, results: list[int]):
-        self._lotto_type = type
-        self._draw_date = date
-        self._results = results
-
-    def format_lotto(self):
-        return f'{self._draw_date}, {self._results}'
